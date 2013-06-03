990b7271a886ef306e71face847389d05b5b96b9
mrmoth <themrmoth@gmail.com>
2013-06-02 23:00:59 -0700

    oh my print statements that shouldn't be there

--- modules.py
+++ modules.py
@@ -1,7 +1,6 @@
 #I'm just throwing this shit here to make it easier to like check things.
 def checkInbox(apihandle):
     messages = apihandle.request("inbox", sort="unread")[u'response'][u'messages']
-    print messages
     #already read, don't need to send a new txt
     #variables to make it look pretty c:
     unreadConvId = messages[0][u'convId']
