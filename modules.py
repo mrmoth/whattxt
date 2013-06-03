e09afaa6bfc9b42e74393702f499b69662d49127
mrmoth <themrmoth@gmail.com>
2013-06-02 22:32:02 -0700

    fixed function

--- modules.py
+++ modules.py
@@ -1,5 +1,5 @@
 #I'm just throwing this shit here to make it easier to like check things.
-def checkInbox():
+def checkInbox(apihandle):
     messages = apihandle.request("inbox", sort="unread")[u'response'][u'messages']
     print messages
     #already read, don't need to send a new txt
