85a3225d956c3c964414ef8a21debdb97d9ddfd3
mrmoth <themrmoth@gmail.com>
2013-06-02 21:55:02 -0700

    doesn't really need an unread status if it's just checking ID's

--- main.py
+++ main.py
@@ -14,15 +14,12 @@ apihandle = whatapi.WhatAPI(username=settings[0], password=settings[1])
 def checkInbox():
 
     messages = apihandle.request("inbox", sort="unread")[u'response'][u'messages']
-    if not messages[0][u'unread']:
-        return None
     unread_id = messages[0][u'convId']
     message = apihandle.request("inbox", type="viewconv", id=unread_id)
     username = messages[0][u'username']
     subject = messages[0][u'subject']
     body = message[u'response'][u'messages'][-1][u'body']
     return (username, subject, body, unread_id)
-print checkInbox()
 
 print "Ok, checking your inbox!"
 prevId = 0
@@ -30,5 +27,12 @@ while True:
     newMessage = checkInbox()
     if newMessage != None and prevId != newMessage[3]:
         prevId = newMessage[3]
-        voice.send_sms(int(settings[2]), "%s - %s\n%s" % newMessage)
+        messageText = "%s - %s\n%s" % newMessage[0:3]
+
+        print "Sending a message"
+
+        voice.send_sms(int(settings[2]), messageText)
+
     time.sleep(10)
+
+
