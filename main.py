837e8e767ea89f285183200a56dc4c1a657d1a41
mrmoth <themrmoth@gmail.com>
2013-06-02 22:31:31 -0700

    splitting things up to make it easier to test

--- main.py
+++ main.py
@@ -12,13 +12,19 @@ apihandle = whatapi.WhatAPI(username=settings[0], password=settings[1])
 
 
 def checkInbox():
-
     messages = apihandle.request("inbox", sort="unread")[u'response'][u'messages']
+
+    #already read, don't need to send a new txt
+    if not messages[0][u'unread']:
+        return None
+
+    #variables to make it look pretty c:
     unread_id = messages[0][u'convId']
     message = apihandle.request("inbox", type="viewconv", id=unread_id)
     username = messages[0][u'username']
     subject = messages[0][u'subject']
     body = message[u'response'][u'messages'][-1][u'body']
+
     return (username, subject, body, unread_id)
 
 print "Ok, checking your inbox!"
@@ -29,7 +35,7 @@ while True:
         prevId = newMessage[3]
         messageText = "%s - %s\n%s" % newMessage[0:3]
 
-        print "Sending a message"
+        print "Sending message: ", messageText
 
         voice.send_sms(int(settings[2]), messageText)
 
---
+++ modules.py
@@ -0,0 +1,16 @@
+#I'm just throwing this shit here to make it easier to like check things.
+def checkInbox():
+    messages = apihandle.request("inbox", sort="unread")[u'response'][u'messages']
+    print messages
+    #already read, don't need to send a new txt
+    if not messages[0][u'unread']:
+        return None
+
+    #variables to make it look pretty c:
+    unread_id = messages[0][u'convId']
+    message = apihandle.request("inbox", type="viewconv", id=unread_id)
+    username = messages[0][u'username']
+    subject = messages[0][u'subject']
+    body = message[u'response'][u'messages'][-1][u'body']
+
+    return (username, subject, body, unread_id)
