a8bbedbef28d29316c14ebb6468f96ce4a49b208
mrmoth <themrmoth@gmail.com>
2013-06-02 22:50:39 -0700

    remove portion of if statement until I figure out why messages are being labeled unread before I read them. It's probably actually because this script is reading them. hm.

--- main.py
+++ main.py
@@ -1,37 +1,23 @@
 import whatapi
 from googlevoice import Voice
+from modules import checkInbox
+import time
 
 config = open ('settings.conf', 'r')
 settings = config.read().split('\n')
 voice = Voice()
-import time
-voice.login()
+#voice.login()
 
 #access what.CD API
 apihandle = whatapi.WhatAPI(username=settings[0], password=settings[1])
 
 
-def checkInbox():
-    messages = apihandle.request("inbox", sort="unread")[u'response'][u'messages']
-
-    #already read, don't need to send a new txt
-    if not messages[0][u'unread']:
-        return None
-
-    #variables to make it look pretty c:
-    unread_id = messages[0][u'convId']
-    message = apihandle.request("inbox", type="viewconv", id=unread_id)
-    username = messages[0][u'username']
-    subject = messages[0][u'subject']
-    body = message[u'response'][u'messages'][-1][u'body']
-
-    return (username, subject, body, unread_id)
 
 print "Ok, checking your inbox!"
 prevId = 0
 while True:
-    newMessage = checkInbox()
-    if newMessage != None and prevId != newMessage[3]:
+    newMessage = checkInbox(apihandle)
+    if prevId != newMessage[3]:
         prevId = newMessage[3]
         messageText = "%s - %s\n%s" % newMessage[0:3]
 
